fee9e238da954719cbabf851517464e3792b46ed
meliza021 <189984449+meliza021@users.noreply.github.com>
2025-06-21 15:53:53 -0500

    feat: :sparkles: The third consultation was completed successfully.

--- ejercicios/dql_eventos.sql
+++ ejercicios/dql_eventos.sql
@@ -8,4 +8,19 @@ JOIN cuota_manejo AS cu ON ta.id = cu.tarjeta_id
 JOIN cliente AS cl ON cu.cliente_id = cl.id
 WHERE cu.total_monto > 0;
 
+-- Consultar el historial de pagos de un cliente específico
 
+SELECT *
+from cliente AS cl 
+JOIN pago AS pa ON cl.id = ps.cliente_id
+JOIN historial_pagoS AS hi ON pa.id = hi.pago_id
+WHERE cl.nombre = 'Juan Perez';
+
+-- Obtener el total de cuotas de manejo pagadas durante un mes determinado.
+
+SELECT 
+    SUM(hi.total) AS total_cuotas_pagadas
+FROM cliente AS cl
+JOIN pago AS pa ON cl.id = pa.cliente_id
+JOIN historial_pagos AS hi ON pa.id = hi.pago_id
+WHERE YEAR(hi.fecha) = 2023;
